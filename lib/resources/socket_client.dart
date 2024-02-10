60c0e77575061f89df0f65ee1ba8c1940eda9823
apaparun <arunprasanth488@gmail.com>
2024-02-10 21:05:56 +0530

    ip changed

--- lib/resources/socket_client.dart
+++ lib/resources/socket_client.dart
@@ -6,7 +6,7 @@ class SocketClient {
   static SocketClient? _instance;
 
   SocketClient._internal() {
-    socket = IO.io("http://192.168.1.35:3000", <String, dynamic>{
+    socket = IO.io("http://192.168.1.44:3000", <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false
     });
